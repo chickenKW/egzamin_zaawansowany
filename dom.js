f3e7448126f8f73c0d3902c254d363813b239bb9
chickenKW <127610001+chickenKW@users.noreply.github.com>
2023-03-26 18:55:44 +0200

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,11 @@
+const tytulTekstu = document.getElementById("tytul1");
+tytulTekstu.textContent = "Bardzo ciekawy artykuł";
+
+const nagElementu1 = document.getElementById("naglowek1");
+nagElementu1.textContent = "Nagłówek 1 artykułu";
+
+const nagElementu2 = document.getElementById("naglowek2");
+nagElementu2.textContent = "Nagłówek 2 artykułu";
+
+const autorTekstu = document.getElementById("autor");
+autorTekstu.textContent = "Autor tekstu: Wojciech Grygierek";
